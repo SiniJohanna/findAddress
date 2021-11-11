8bc141b5bfb1d706da18d8e268a9bac014335a85
SiniJohanna <82939474+SiniJohanna@users.noreply.github.com>
2021-11-11 15:31:27 +0200

    findAddress

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import MapView, { Marker} from'react-native-maps';
+import { StyleSheet, TextInput, View, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+  const [region, setRegion] = useState(
+    {
+      latitude:60.200692,
+      longitude:24.934302,
+      latitudeDelta:0.0322,
+      longitudeDelta:0.0221,
+    }
+  );
+  const [coordinate, setCoordinate] = useState(
+    {
+      latitude:'',
+      longitude: ''
+    }
+  );
+
+  const getCoordinates = address => {
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=KEY=${address}`)
+    .then(response=>  response.json())
+    .then(responseJson=>{ 
+      setCoordinate(
+        {
+          latitude: responseJson.results[0].locations[0].displayLatLng.lat,
+          longitude: responseJson.results[0].locations[0].displayLatLng.lng
+        }
+      )
+      setRegion(
+        {
+          latitude: responseJson.results[0].locations[0].displayLatLng.lat,
+          longitude: responseJson.results[0].locations[0].displayLatLng.lng,
+          latitudeDelta:0.0322,
+          longitudeDelta:0.0221,
+        }
+      )
+    })
+    .catch(error=> { Alert.alert('Error',error); });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <MapView
+      style={styles.map}
+      region={region}
+        >
+        <Marker coordinate={coordinate}/>
+        </MapView>
+        <View style={{flex: 1}}>
+        <TextInput 
+        style={styles.input}
+        onChangeText={address=> setAddress(address)}
+        value={address} />
+        <Button onPress={() => getCoordinates(address)} title='Show' />
+      </View>
     </View>
   );
 }
@@ -18,4 +68,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  input: {
+    width:200,
+    borderColor:'gray',
+    borderWidth:1
+  },
+  map: {
+    flex:2,
+    width: "100%",
+    height: "100%"
+  }
+
+  
 });
